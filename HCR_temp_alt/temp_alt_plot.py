5ce3d73fca7502ead31922d2b8bd913bd98d27b4
leavesntwigs <bjav7496@gmail.com>
2025-12-15 16:48:31 -0700

    improving the plots with fonts, legend placement, etc.

--- HCR_temp_alt/temp_alt_plot.py
+++ HCR_temp_alt/temp_alt_plot.py
@@ -1,13 +1,107 @@
 
+# ----  changes needed here ----
+
+YYYYMMDD = '20190822'
+
+# ---- end changes needed ----
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
-temp_data = pd.read_csv('20190822.txt')
+
+# temp_data = pd.read_csv('20190822.txt')
+temp_data = pd.read_csv(YYYYMMDD + '.txt')
 temp_data
-fig, axes = plt.subplots(nrows=5, ncols=1)
-fig.show()
-temp_data.plot(ax=axes[0], x='time', y=['Cmigits3501Altitude'], ylabel='Altitude (?)')
-temp_data.plot(ax=axes[1], x='time', y=['PentekFpgaTemp', 'PentekBoardTemp','Ps28VTemp'], ylabel='Temperature [C]', xlabel='20190822')
-temp_data.plot(ax=axes[2], x='time', y=['VLnaTemp', 'HLnaTemp', 'XmitterTemp'], ylabel='Temperature [C]', xlabel='20190822')
-temp_data.plot(ax=axes[3], x='time', y=['RotationMotorTemp', 'TiltMotorTemp', 'EikTemp', 'PolarizationSwitchTemp', 'RfDetectorTemp', 'NoiseSourceTemp', 'PloTemp', 'RdsInDuctTemp'],  ylabel='Temperature [C]', xlabel='20190822')
-temp_data.plot(ax=axes[4], x='time', y=['InsTemp','TailconeTemp'],  ylabel='Temperature [C]', xlabel='20190822')
-fig.show()
+
+fig, axes = plt.subplots(nrows=5, ncols=1, 
+   # sharex=True
+)
+
+# Set spacing to zero
+plt.subplots_adjust(wspace=0, hspace=0)
+
+# fig.show()
+
+temp_data.plot(ax=axes[0], x='time', y=['Cmigits3501Altitude'], ylabel='Altitude (?)',
+   xticks=[],
+   xlabel="",
+   # legend=[loc="lower right"],
+)
+axes[0].legend(
+   fontsize=6,
+   #loc="lower right",
+   loc="best",
+   # ncol=len(yfields),
+)
+
+yfields = ['PentekFpgaTemp', 'PentekBoardTemp','Ps28VTemp']
+temp_data.plot(ax=axes[1], x='time', 
+   y=yfields, 
+   ylabel='Temp [C]', 
+   xticks=[],
+   xlabel="",
+)
+axes[1].legend(
+   fontsize=6,
+   #loc="lower right",
+   loc="best",
+   ncol=len(yfields),
+   labels=[s.replace('Temp','') for s in yfields],
+)
+
+yfields = ['VLnaTemp', 'HLnaTemp', 'XmitterTemp']
+temp_data.plot(ax=axes[2], x='time', 
+   y=yfields,
+   ylabel='Temp [C]', 
+   xticks=[],
+   xlabel="",
+)
+axes[2].legend(
+   fontsize=6,
+   #loc="lower right",
+   loc="best",
+   ncol=len(yfields),
+   labels=[s.replace('Temp','') for s in yfields],
+)
+
+yfields = ['RotationMotorTemp', 'TiltMotorTemp', 'EikTemp', 'PolarizationSwitchTemp', 'RfDetectorTemp', 'NoiseSourceTemp', 'PloTemp', 'RdsInDuctTemp']
+temp_data.plot(ax=axes[3], x='time', 
+   y=yfields,
+   ylabel='Temp [C]', 
+   ylim=[-5,55],
+   xticks=[],
+   xlabel="",
+)
+axes[3].legend(
+   fontsize=4,
+   loc="best",
+   ncol=len(yfields),
+   labels=[s.replace('Temp','') for s in yfields],
+)
+
+yfields = ['InsTemp','TailconeTemp']
+temp_data.plot(ax=axes[4], x='time', 
+   y=yfields,  
+   ylabel='Temp [C]',
+   xlabel=YYYYMMDD + ' time',
+   fontsize=8,
+)
+axes[4].legend(
+   fontsize=6,
+   #loc="lower right",
+   loc="best",
+   ncol=len(yfields),
+   labels=[s.replace('Temp','') for s in yfields],
+)
+# fig.show()
+plt.ylabel('Temp [C]', fontsize = 8)
+
+plt.suptitle('OTREC')
+
+# fig.rcParams["axes.labelsize"] = 4 
+# Iterate through all Axes objects and add vertical grid lines
+for ax in axes:
+   ax.grid(axis='x') # , linestyle='--', alpha=0.6) # Customize linestyle and transparency
+
+
+plt.savefig(YYYYMMDD + '.png')
